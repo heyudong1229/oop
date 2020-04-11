669840b4da49a146dd766334c0fb43c1a164d4ff
heyudong <517460595@qq.com>
2020-04-11 16:35:31 +0800

    项目第一次构建ooop

---
+++ src/main/java/ApplicationMain.java
@@ -0,0 +1,7 @@
+/**
+ * @Author heyudong
+ * @create 2020/4/11 16:22
+ */
+
+public class ApplicationMain {
+}
---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * @Author heyudong
+ * @create 2020/4/11 16:21
+ */
+public class HelloController {
+    public void a(){
+        System.out.println("123");
+    }
+}
